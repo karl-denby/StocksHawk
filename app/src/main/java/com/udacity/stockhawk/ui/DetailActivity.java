c685cdb88d3e4d63dd3d63e14e394bd6e3147521
karl-denby <karl.denby@gmail.com>
2017-04-10 22:13:39 +0100

    use asyncloader to get stock details

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -1,10 +1,15 @@
 package com.udacity.stockhawk.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.udacity.stockhawk.R;
 
@@ -20,7 +25,6 @@ import yahoofinance.histquotes.HistoricalQuote;
 
 public class DetailActivity extends AppCompatActivity {
 
-
     @SuppressWarnings("WeakerAccess")
     @BindView(R.id.detail_stock_name)
     TextView tvStockName;
@@ -28,15 +32,17 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_phone_detail);
+
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String symbol = intent.getStringExtra("symbol");
+        new StockDetailsAsyncTask().execute(symbol);
 
         Timber.d("Detail activity for stock: %s created", symbol);
-
+    }
+        /*
         Stock stock = null;
         List<HistoricalQuote> stockHistQuotes = null;
         try {
@@ -48,6 +54,30 @@ public class DetailActivity extends AppCompatActivity {
 
         tvStockName.setText(symbol);
         Timber.d(stockHistQuotes.toString());
+        */
+
+    private class StockDetailsAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            Stock stock = null;
+            List<HistoricalQuote> stockHistQuotes = null;
+            try {
+                stock = YahooFinance.get(strings[0]);
+                stockHistQuotes = stock.getHistory();
+            } catch (IOException e) {
+                Timber.d(e.toString());
+            }
+
+            return stockHistQuotes.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            tvStockName.setText(s);
+        }
     }
 
 }

71caf9d04bcde6eec81e05343ee40fd862dacfbc
karl-denby <karl.denby@gmail.com>
2017-04-23 20:00:35 +0100

    general tidy up pre-overhaul

---
+++ app/src/main/java/com/android/example/stockshawk/StocksHawkApp.java
@@ -0,0 +1,20 @@
+package com.android.example.stockshawk;
+
+import android.app.Application;
+
+import timber.log.BuildConfig;
+import timber.log.Timber;
+
+public class StocksHawkApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.uprootAll();
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+}
---
+++ app/src/main/java/com/android/example/stockshawk/data/DbHelper.java
@@ -0,0 +1,40 @@
+package com.android.example.stockshawk.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class DbHelper extends SQLiteOpenHelper {
+
+
+    private static final String NAME = "StocksHawk.db";
+    private static final int VERSION = 2;
+
+
+    DbHelper(Context context) {
+        super(context, NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String builder = "CREATE TABLE " + Contract.Quote.TABLE_NAME + " ("
+                + Contract.Quote._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Contract.Quote.COLUMN_SYMBOL + " TEXT NOT NULL, "
+                + Contract.Quote.COLUMN_PRICE + " REAL NOT NULL, "
+                + Contract.Quote.COLUMN_ABSOLUTE_CHANGE + " REAL NOT NULL, "
+                + Contract.Quote.COLUMN_PERCENTAGE_CHANGE + " REAL NOT NULL, "
+                + Contract.Quote.COLUMN_HISTORY + " TEXT NOT NULL, "
+                + "UNIQUE (" + Contract.Quote.COLUMN_SYMBOL + ") ON CONFLICT REPLACE);";
+
+        db.execSQL(builder);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL(" DROP TABLE IF EXISTS " + Contract.Quote.TABLE_NAME);
+
+        onCreate(db);
+    }
+}

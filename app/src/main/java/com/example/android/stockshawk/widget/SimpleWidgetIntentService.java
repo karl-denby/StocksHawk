0ed5b2c451cf9db9cec3fb94afc4e350c003dd8c
karl-denby <karl.denby@gmail.com>
2017-04-27 21:28:36 +0100

    Widget is resizeable and loads a listview for data or image if no data is available.

--- app/src/main/java/com/example/android/stockshawk/widget/SimpleWidgetIntentService.java
+++ app/src/main/java/com/example/android/stockshawk/widget/SimpleWidgetIntentService.java
@@ -21,8 +21,6 @@ public class SimpleWidgetIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
 
-        Log.v("DEBUG", "handling intent...");
-
         // Retrieve all of the Today widget ids: these are the widgets we need to update
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
         int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, SimpleWidgetProvider.class));
@@ -31,15 +29,14 @@ public class SimpleWidgetIntentService extends IntentService {
         for (int appWidgetId: appWidgetIds) {
             RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget_simple);
 
-            views.setTextViewText(R.id.widget_text, "passed");
+            views.setEmptyView(R.id.widget_list_view, R.id.no_data);
 
             Intent launchIntent = new Intent(this, PhoneActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
             views.setOnClickPendingIntent(R.id.widget, pendingIntent);
 
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list_view);
             appWidgetManager.updateAppWidget(appWidgetId, views);
-
-            Log.v("DEBUG", "updating widgets...");
         }
     }
 

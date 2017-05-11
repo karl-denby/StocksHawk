4116f53cc31abd757da1840cfa3e3b3baa681ae6
karl-denby <karl.denby@gmail.com>
2017-05-11 21:48:11 +0100

    WIP: fragment implements a callback and fragmentmanager/switch pattern

--- app/src/main/java/com/example/android/stockshawk/ui/MasterDetail.java
+++ app/src/main/java/com/example/android/stockshawk/ui/MasterDetail.java
@@ -1,14 +1,19 @@
 package com.example.android.stockshawk.ui;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.android.stockshawk.R;
 
 import butterknife.ButterKnife;
 
 
-public class MasterDetail extends Activity {
+public class MasterDetail extends Activity implements SymbolFragment.OnClickListener {
+
+    Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,4 +23,44 @@ public class MasterDetail extends Activity {
         ButterKnife.bind(this);
     }
 
+    @Override
+    public void OnClick(View view) {
+        // The user selected the headline of an article from the HeadlinesFragment
+        // Do something here to display that article
+
+        DetailsFragment detailsFragment = (DetailsFragment)
+                getFragmentManager().findFragmentById(R.id.details_frag);
+
+        if (mToast!=null) {
+            mToast.cancel();
+        }
+
+        if (detailsFragment!= null) {
+            // If detail frag is available, we're in two-pane/tablet layout...
+            mToast = Toast.makeText(this, "Tablet Layout", Toast.LENGTH_SHORT);
+
+        } else {
+            // Otherwise, we're in the one-pane layout and must swap frags...
+            mToast = Toast.makeText(this, "Phone Layout", Toast.LENGTH_SHORT);
+
+            // Create fragment and give it an argument for the selected article
+            DetailsFragment newFragment = new DetailsFragment();
+            Bundle args = new Bundle();
+
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.list_frag, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        }
+
+        if (mToast!=null) {
+            mToast.show();
+        }
+
+    }
 }
--- app/src/main/java/com/example/android/stockshawk/ui/SymbolFragment.java
+++ app/src/main/java/com/example/android/stockshawk/ui/SymbolFragment.java
@@ -1,21 +1,62 @@
 package com.example.android.stockshawk.ui;
 
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.example.android.stockshawk.R;
 
 public class SymbolFragment extends Fragment {
+
+    OnClickListener mCallback;
+    FrameLayout frameLayout;
+
+    public interface OnClickListener {
+        public void OnClick(View view);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.symbol_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        frameLayout = (FrameLayout) getView().findViewById(R.id.symbol_frame);
+        if (frameLayout!=null) {
+            frameLayout.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    mCallback.OnClick(view);
+                }
+            });
+        } else {
+            Log.v("DEBUG", "Didn't find framelayout reference");
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnClickListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnClickListener");
+        }
     }
 
 }
